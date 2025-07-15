378ee96d1cc241a7f77cc1c0ee437cd2f5945527
Nurdanchik <nysanovnurdan33@gmail.com>
2025-07-15 18:15:48 +0600

    still tryna fix media

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from config import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +14,6 @@ urlpatterns = [
     path('api/feedbacks/', include('feedbacks.urls')),
     path('api/products/', include('products.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
